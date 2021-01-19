6dacd680f36c836a3f7d85bf56826355c57955a0
Pierre <pierre.kieffer@hubstairs.com>
2021-01-19 14:32:00 +0100

    update client

--- client/client.go
+++ client/client.go
@@ -7,7 +7,7 @@ import (
 	"log"
 )
 
-func InitPubSubClient(ctx context.Context, projectID, credFile ...string) (*pubsub.Client, error) {
+func InitPubSubClient(ctx context.Context, projectID string, credFile ...string) (*pubsub.Client, error) {
 
 	if len(credFile) > 0 {
 		client, err := pubsub.NewClient(ctx, projectID, option.WithCredentialsFile(credFile))
