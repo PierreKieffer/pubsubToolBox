2ed16f3f31667c2b6f239716e6fda43316eaea6c
PierreKieffer <38128948+PierreKieffer@users.noreply.github.com>
2022-03-31 14:23:00 +0200

    feat : consumer handler to process attributes

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,43 +1,61 @@
-package consumer
+package consumer                                                                                                                                                                                                                                                                
 
 import (
-	"context"
-	"log"
-	"os"
+        "context"
+        "log"
+        "os"
 
-	"cloud.google.com/go/pubsub"
+        "cloud.google.com/go/pubsub"
 )
 
-func InitBuffer(bufferSize int) chan string {
-	var buffer = make(chan string, bufferSize)
-	return buffer
+type Message struct {
+        Data       string
+        Attributes map[string]string
 }
 
-func Pull(ctx context.Context, pubsubClient *pubsub.Client, subName, topicID string, buffer chan string) {
+type Consumer struct {
+        Context        context.Context
+        PubSubClient   *pubsub.Client
+        SubscriberName string
+        TopicID        string
+        Buffer         chan Message
+}
 
-	topic := pubsubClient.Topic(topicID)
+func InitBuffer(bufferSize int) chan Message {
+        var buffer = make(chan Message, bufferSize)
+        return buffer
+}
 
-	// Create topic subscription if it does not yet exist.
-	sub := pubsubClient.Subscription(subName)
-	exists, err := sub.Exists(ctx)
-	if err != nil {
-		log.Println("ERROR : consumer.Pull : " + err.Error())
-		os.Exit(1)
-	}
-	if !exists {
-		if _, err = pubsubClient.CreateSubscription(ctx, subName, pubsub.SubscriptionConfig{Topic: topic}); err != nil {
-			log.Println("ERROR : consumer.Pull : " + err.Error())
-			os.Exit(1)
-		}
-	}
+func (c *Consumer) Pull() {
 
-	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
-		buffer <- string(msg.Data)
-		log.Println("INFO : consumer.Pull : Message received and pushed to buffer")
-		msg.Ack()
-	})
-	if err != nil {
-		log.Println("ERROR : consumer.Pull : " + err.Error())
-		os.Exit(1)
-	}
+        topic := c.PubSubClient.Topic(c.TopicID)
+
+        // Create topic subscription if it does not yet exist.
+        sub := c.PubSubClient.Subscription(c.SubscriberName)
+        exists, err := sub.Exists(c.Context)
+        if err != nil {
+                log.Println("ERROR : consumer.Pull : " + err.Error())
+                os.Exit(1)
+        }   
+        if !exists {
+                if _, err = c.PubSubClient.CreateSubscription(c.Context, c.SubscriberName, pubsub.SubscriptionConfig{Topic: topic}); err != nil {
+                        log.Println("ERROR : consumer.Pull : " + err.Error())
+                        os.Exit(1)
+                }   
+        }   
+
+        err = sub.Receive(c.Context, func(ctx context.Context, msg *pubsub.Message) {
+                message := Message{
+                        Data:       string(msg.Data),
+                        Attributes: msg.Attributes,
+                }   
+
+                c.Buffer <- message
+                log.Println("INFO : consumer.Pull : Message received and pushed to buffer")
+                msg.Ack()
+        })  
+        if err != nil {
+                log.Println("ERROR : consumer.Pull : " + err.Error())
+                os.Exit(1)
+        }   
 }
